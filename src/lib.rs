84f1f5ed8240ec378277fbc00b3c3d5e8420f810
Tomas Longo <longo.tomas@googlemail.com>
2021-01-08 15:56:28 +0100

    Modularize app

---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+use rusqlite::{Connection, Result};
+use rusqlite::{NO_PARAMS, MappedRows, types::FromSql, types::FromSqlResult, types::ValueRef};
+
+#[derive(Debug)]
+pub struct ReadingEntry {
+    pub id: i64,
+    pub author: String,
+    pub title: String,
+    pub genre: String,
+    pub format: Format,
+    pub status: String,
+    pub tags: Vec<String>
+}
+
+#[derive(Debug)]
+pub enum Format {
+    Book,
+    Kindle
+}
+
+impl FromSql for Format {
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+        match value.as_str() {
+            Ok(s) => {
+                return Ok(Format::Book);
+            },
+            _ => return Ok(Format::Kindle),
+        }
+    }
+}
+
+pub fn stringToFormatEnum(toConvert: &str) -> Format {
+    let lc = toConvert.to_lowercase();
+    if lc == "book" {
+        return Format::Book;
+    } else if lc == "kindle" {
+        return Format::Kindle
+    } else {
+        return Format::Book
+    }
+}
+
+pub fn formatEnumToString(f: &Format) -> String {
+    match f {
+        Format::Book => return String::from("book"),
+        Format::Kindle => return String::from("kindle")
+    };
+}
+
+pub trait Backend {
+    fn addEntry(&self, e: &ReadingEntry);
+    fn getAllEntries(&self) -> Result<Vec<ReadingEntry>>;
+}
+
+pub struct SqliteBackend {
+    conn: Connection
+}
+
+impl SqliteBackend {
+    pub fn new(db_name: &str) -> Result<SqliteBackend> {
+        let conn = Connection::open(db_name)?;
+        conn.execute(
+            " create table if not exists reading_entries (
+                id integer primary key autoincrement,
+                title text not null default '',
+                author text not null default '',
+                genre text  not null default '',
+                format text not null default '',
+                tags text not null default '',
+                status text not null default '',
+                created_at timestamp default current_timestamp,
+                updated_at timestamp default current_timestamp
+                );
+            ", NO_PARAMS,)?;
+
+        return Ok(SqliteBackend {
+            conn
+        })
+    }
+}
+
+impl Backend for SqliteBackend {
+    fn addEntry(&self, re: &ReadingEntry) {
+        let insertString = "insert into reading_entries (title, author, genre, format, tags, status) values (?1, ?2, ?3, ?4, ?5, ?6);";
+        self.conn.execute(insertString, &[&re.title, &re.author, &re.genre, &formatEnumToString(&re.format), &re.tags.join(" "), &re.status]);
+    }
+
+    fn getAllEntries(&self) -> Result<Vec<ReadingEntry>> {
+        let mut stmt = self.conn.prepare("SELECT id, title, author, genre, format, tags, status FROM reading_entries")?;
+
+        let entries = stmt.query_map(NO_PARAMS, |row| {
+
+            let tags_from_db: String = row.get(5)?;
+            let splits = tags_from_db.split(" ");
+
+            // gibt es hierfuer nicht schon was in der std lib?
+            let mut vec: Vec<String> = Vec::new();
+            for s in splits {
+                vec.push(String::from(s));
+            }
+
+            return Ok(ReadingEntry {
+                id: row.get(0)?,
+                title: row.get(1)?,
+                author: row.get(2)?,
+                genre: row.get(3)?,
+                format: row.get(4)?,
+                status: row.get(6)?,
+                tags: vec
+            })
+        })?;
+
+        let mut entryList = Vec::new();
+        for e in entries {
+            entryList.push(e?);
+        }
+
+        return Ok(entryList);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use rusqlite::{NO_PARAMS, MappedRows, types::FromSql, types::FromSqlResult, type
 use std::io;
 use std::io::Write;
 
+use readinglist::{Backend, SqliteBackend, ReadingEntry, Format, formatEnumToString, stringToFormatEnum};
+
 use comfy_table::Table;
 use comfy_table::presets::UTF8_FULL;
 
@@ -18,52 +20,6 @@ struct Cli {
 #[derive(StructOpt)]
 enum Command {
     Add
-}
-
-#[derive(Debug)]
-enum Format {
-    Book,
-    Kindle
-}
-
-impl FromSql for Format {
-    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
-        match value.as_str() {
-            Ok(s) => {
-                return Ok(Format::Book);
-            },
-            _ => return Ok(Format::Kindle),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct ReadingEntry {
-    id: i64,
-    author: String,
-    title: String,
-    genre: String,
-    format: Format,
-    status: String,
-    tags: Vec<String>
-}
-
-fn stringToFormatEnum(toConvert: &str) -> Format {
-    let lc = toConvert.to_lowercase();
-    if lc == "book" {
-        return Format::Book;
-    } else if lc == "kindle" {
-        return Format::Kindle
-    } else {
-        return Format::Book
-    }
-}
-
-fn formatEnumToString(f: &Format) -> String {
-    match f {
-        Format::Book => return String::from("book"),
-        Format::Kindle => return String::from("kindle")
-    };
 }
 
 fn add() -> ReadingEntry {
@@ -95,68 +51,11 @@ fn add() -> ReadingEntry {
     return re;
 }
 
-fn query_reading_list(conn: &Connection) -> Result<Vec<ReadingEntry>> {
-    let mut stmt = conn.prepare("SELECT id, title, author, genre, format, tags, status FROM reading_entries")?;
-
-    let entries = stmt.query_map(NO_PARAMS, |row| {
-
-        let tags_from_db: String = row.get(5)?;
-        let splits = tags_from_db.split(" ");
-
-        // gibt es hierfuer nicht schon was in der std lib?
-        let mut vec: Vec<String> = Vec::new();
-        for s in splits {
-            vec.push(String::from(s));
-        }
-
-        return Ok(ReadingEntry {
-            id: row.get(0)?,
-            title: row.get(1)?,
-            author: row.get(2)?,
-            genre: row.get(3)?,
-            format: row.get(4)?,
-            status: row.get(6)?,
-            tags: vec
-        })
-    })?;
-
-    let mut entryList = Vec::new();
-    for e in entries {
-        entryList.push(e?);
-    }
-
-    return Ok(entryList);
-}
-
 fn create_promt_for(item: &str) -> String {
     return Input::with_theme(&ColorfulTheme::default())
         .with_prompt(item)
         .interact_text()
         .unwrap();
-}
-
-fn init_db(db_name: &str) -> Result<Connection> {
-    let conn = Connection::open(db_name)?;
-    conn.execute(
-        " create table if not exists reading_entries (
-            id integer primary key autoincrement,
-            title text not null default '',
-            author text not null default '',
-            genre text  not null default '',
-            format text not null default '',
-            tags text not null default '',
-            status text not null default '',
-            created_at timestamp default current_timestamp,
-            updated_at timestamp default current_timestamp
-            );
-        ", NO_PARAMS,)?;
-
-    return Ok(conn);
-}
-
-fn insertReadingEntry(re: &ReadingEntry, conn: &Connection) {
-    let insertString = "insert into reading_entries (title, author, genre, format, tags, status) values (?1, ?2, ?3, ?4, ?5, ?6);";
-    conn.execute(insertString, &[&re.title, &re.author, &re.genre, &formatEnumToString(&re.format), &re.tags.join(" "), &re.status]);
 }
 
 fn print_table(entries: &Vec<ReadingEntry>) -> Table {
@@ -181,14 +80,15 @@ fn print_table(entries: &Vec<ReadingEntry>) -> Table {
 fn main() -> Result<()>{
     let args = Cli::from_args();
 
-    let dbConn = init_db("./readinglist.db")?;
+    // let dbConn = init_db("./readinglist.db")?;
+    let backend = SqliteBackend::new("./readinglist.db")?;
     match args.cmd {
         Some(Command::Add) => {
             let re = add();
-            insertReadingEntry(&re, &dbConn)
+            backend.addEntry(&re)
         },
         None => {
-            let entries = query_reading_list(&dbConn)?;
+            let entries = backend.getAllEntries()?;
 
             println!("{}", print_table(&entries));
         }
